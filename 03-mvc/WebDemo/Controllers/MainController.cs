40cfc65fc4da12e6fdd2e6ea6c5a3232c863a493
heidi1105 <heidi1105@gmail.com>
2024-06-05 14:55:55 -0600

    WebDemo 2

--- 03-mvc/WebDemo/Controllers/MainController.cs
+++ 03-mvc/WebDemo/Controllers/MainController.cs
@@ -18,6 +18,40 @@ public class MainController : Controller
     [HttpGet("lucky/{num}")]
     public ViewResult LuckyPage(int num)
     {
+        ViewBag.Num = num;
+        return View();
+    }
+
+    [HttpGet("trymyluck")]
+    public RedirectResult MoreThanTen()
+    {
+        int luckyNum = new Random().Next(10, 100);
+        return Redirect($"/lucky/{luckyNum}"); // correspond to the URL
+    }
+
+    [HttpGet("tryagain")]
+    public RedirectToActionResult LessThanTen()
+    {
+        int luckyNum = new Random().Next(10);
+        // Correspond to the Action
+        return RedirectToAction("LuckyPage", new {num = luckyNum});
+    }
+
+    [HttpGet("raffle/entry")]
+    public ViewResult RaffleEntry()
+    {
+        List<string> allHoros = ["Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius", "Aquarius", "Pisces"];
+        ViewBag.AllHoros = allHoros;
+        return View();
+    }
+
+    [HttpPost("raffle/process")] // matching the form action
+    public ViewResult RaffleResult(string fullName, string horoscope, string introduction)
+    {   
+        ViewBag.Winning = new Random().NextSingle();
+        ViewBag.FullName = fullName;
+        ViewBag.Horoscope = horoscope;
+        ViewBag.Introduction = introduction;
         return View();
     }
 
--- 03-mvc/WebDemo/Program.cs
+++ 03-mvc/WebDemo/Program.cs
@@ -10,7 +10,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Main}/{action=Index}/{id?}");
 
     
 app.Run();
